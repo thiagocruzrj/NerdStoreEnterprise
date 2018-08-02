985930cb08da149e771e9ff1b085c54eb7e9ff5d
Thiago Cruz <thagocruz@gmail.com>
2018-08-02 05:11:42 -0300

    Add files via upload

---
+++ Arrays/flatMap.js
@@ -0,0 +1,32 @@
+const escola = [{
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    },{
+        nome: 'Ana',
+        nota: 9.3
+    }]
+},{
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    },{
+        nome: 'Roberto',
+        nota: 7.3
+    }]
+}]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turmas => turmas.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma) //varrer cada escola da turma
+console.log(notas1)
+
+Array.prototype.flatMap = function(callback) {
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const notas2 = escola.flatMap(getNotasDaTurma)
+console.log(notas2)
---
+++ Arrays/reduce1.js
@@ -0,0 +1,13 @@
+const alunos = [
+    {nome: 'joao', nota: 7.3, bolsista: false},
+    {nome: 'Maria', nota: 9.2, bolsista: true},
+    {nome: 'Pedro', nota: 9.8, bolsista: false},
+    {nome: 'Ana', nota: 8.7, bolsista: true}
+]
+
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, valorAtual){
+    console.log(acumulador, valorAtual)
+    return acumulador + valorAtual
+}, 10 /*Valor inicial caso necessite */)
+
+console.log(resultado)

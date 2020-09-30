bddd523fe9c773eff91cbfff08b4cf4cb68453e4
Thiago <thagocruz@gmail.com>
2020-09-29 23:08:57 -0300

    Add app context

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API
 {
@@ -17,6 +19,7 @@ namespace NSE.Identidade.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 

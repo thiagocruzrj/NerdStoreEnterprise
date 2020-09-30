178b3b894efb8854fd7a65e23bb33760ecdfdd14
Thiago <thagocruz@gmail.com>
2020-09-29 23:37:32 -0300

    Create AuthController.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+
+        }
+
+        public async Task<ActionResult> Registrar(UsuarioLogin usuarioLogin)
+        {
+
+        }
+    }
+}

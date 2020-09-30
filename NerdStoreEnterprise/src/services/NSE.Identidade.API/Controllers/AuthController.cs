230d023a0503baa14cc1ca0fd977962349389dff
Thiago <thagocruz@gmail.com>
2020-09-29 23:40:55 -0300

    Add base constructor

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Identidade.API.Models;
 using System.Threading.Tasks;
 
@@ -6,12 +7,29 @@ namespace NSE.Identidade.API.Controllers
 {
     public class AuthController : Controller
     {
-        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
-        {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userInManager;
 
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userInManager)
+        {
+            _signInManager = signInManager;
+            _userInManager = userInManager;
         }
 
-        public async Task<ActionResult> Registrar(UsuarioLogin usuarioLogin)
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+        }
+
+        public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
         {
 
         }

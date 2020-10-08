94bcfdae03235b3696c7d60ccba81fd3f6953a9d
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 16:44:36 -0300

    Create HttpClientAuthorizationDelegatingHandler.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,15 @@
+﻿using NSE.WebApp.MVC.Extensions;
+using System.Net.Http;
+
+namespace NSE.WebApp.MVC.Services.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+    }
+}

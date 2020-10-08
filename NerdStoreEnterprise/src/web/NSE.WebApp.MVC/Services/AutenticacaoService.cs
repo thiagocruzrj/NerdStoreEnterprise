f4b403cf85d016be35ea2ac708367110005c68a9
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:17:50 -0300

    Create AutenticacaoService.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class AutenticacaoService : Service, IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient,
+                                   IOptions<AppSettings> settings)
+        {
+            httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
+            _httpClient = httpClient;
+        }
+    }
+}

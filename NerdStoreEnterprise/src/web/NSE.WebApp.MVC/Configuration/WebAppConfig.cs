03951cce67f07df2eb9fe0049334b38f6d3d3ca1
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:27:11 -0300

    Create WebAppConfig.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebApp.MVC.Extensions;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+
+            services.Configure<AppSettings>(configuration);
+        }
+
+    }
+}

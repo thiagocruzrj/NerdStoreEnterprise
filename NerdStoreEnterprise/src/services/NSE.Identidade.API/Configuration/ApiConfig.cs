87343f644f36f0509824dd9cc4c7b7d7391a5ee4
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 13:37:38 -0300

    Add UseApiConfiguration

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace NSE.Identidade.API.Configuration
 {
@@ -7,6 +10,26 @@ namespace NSE.Identidade.API.Configuration
         public static void AddApiConfiguration(this IServiceCollection services)
         {
             services.AddControllers();
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }

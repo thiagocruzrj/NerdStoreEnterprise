daf70e199854e7282d252e00235b3578f8a0e8db
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 16:41:26 -0300

    Add GetUserToken

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/IUser.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -87,5 +87,16 @@ namespace NSE.WebApp.MVC.Extensions
             var claim = principal.FindFirst("email");
             return claim?.Value;
         }
+
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
     }
 }

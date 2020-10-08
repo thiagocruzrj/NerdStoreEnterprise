bb4e963d505fc8e41778b81dd8513ddf7542de79
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:23:06 -0300

    Add TratarErrosResponse

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/Service.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using NSE.WebApp.MVC.Extensions;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -23,6 +24,23 @@ namespace NSE.WebApp.MVC.Services
             };
 
             return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
+        protected bool TratarErrosResponse(HttpResponseMessage response)
+        {
+            switch ((int)response.StatusCode)
+            {
+                case 401:
+                case 403:
+                case 404:
+                case 500:
+                    throw new CustomHttpRequestException(response.StatusCode);
+                case 400:
+                    return false;
+            }
+
+            response.EnsureSuccessStatusCode();
+            return true;
         }
     }
 }

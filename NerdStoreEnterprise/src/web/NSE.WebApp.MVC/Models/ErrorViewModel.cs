994151aad267969e82ea098878072ae3923510d8
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 16:26:10 -0300

    Update ErrorViewModel.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -1,11 +1,11 @@
-using System;
+using System.Collections.Generic;
 
 namespace NSE.WebApp.MVC.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
     }
 }

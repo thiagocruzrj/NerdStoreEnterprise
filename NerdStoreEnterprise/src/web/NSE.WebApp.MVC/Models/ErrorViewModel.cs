78685a3ad9d285cd4ea4b186a66740e04a1f88b6
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 16:26:21 -0300

    Add ResponseResult

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -8,4 +8,11 @@ namespace NSE.WebApp.MVC.Models
         public string Titulo { get; set; }
         public string Mensagem { get; set; }
     }
+
+    public class ResponseResult
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+    }
 }

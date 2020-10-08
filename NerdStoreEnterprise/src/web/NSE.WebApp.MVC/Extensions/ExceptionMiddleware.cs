2ddd9ee6b4b4bf30b2849ec4ec4d2130cf408d00
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 16:35:09 -0300

    Create ExceptionMiddleware.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+    }
+}

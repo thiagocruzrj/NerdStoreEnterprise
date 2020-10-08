e2e4072e953cc76b55aa5902aec2811481ae364b
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:22:22 -0300

    Create Service.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public abstract class Service
+    {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(
+                JsonSerializer.Serialize(dado),
+                Encoding.UTF8,
+                "application/json");
+        }
+    }
+}

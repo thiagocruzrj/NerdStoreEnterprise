5accd03a5ded7e8ae8217b148cbc21393a13109f
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:39:09 -0300

    Add RealizarLogin method

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NSE.Identidade.API.Models;
-using NSE.WebApp.MVC.Services;
+﻿using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
 
 namespace NSE.WebApp.MVC.Controllers
 {
@@ -35,6 +40,28 @@ namespace NSE.WebApp.MVC.Controllers
             await RealizarLogin(resposta);
 
             return RedirectToAction("Index", "Catalogo");
+        }
+
+        private async Task RealizarLogin(UsuarioRespostaLogin resposta)
+        {
+            var token = ObterTokenFormatado(resposta.AccessToken);
+
+            var claims = new List<Claim>();
+            claims.Add(new Claim("JWT", resposta.AccessToken));
+            claims.AddRange(token.Claims);
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60),
+                IsPersistent = true
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
         }
 
         private static JwtSecurityToken ObterTokenFormatado(string jwtToken)

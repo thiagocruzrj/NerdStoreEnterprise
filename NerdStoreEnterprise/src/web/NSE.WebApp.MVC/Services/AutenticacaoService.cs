1b288d611d967ee38625387b8a37ffe456bace13
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 17:22:46 -0300

    Update AutenticacaoService.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using NSE.WebApp.MVC.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {

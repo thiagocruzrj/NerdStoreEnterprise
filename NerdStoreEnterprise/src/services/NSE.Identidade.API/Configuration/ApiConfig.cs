479ac17dfb459d6e153bc5a076ba3c8225b2d709
Thiago Cruz <thagocruz@gmail.com>
2020-10-08 13:32:41 -0300

    Create ApiConfig.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+    }
+}

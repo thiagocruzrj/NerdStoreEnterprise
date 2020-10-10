30944ea6d68b3b1b025cf5306102d4cdb5fa3de8
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 16:01:45 -0300

    Create IRepository.cs

---
+++ NerdStoreEnterprise/src/buildingBlocks/NSE.Core/DomainObjects/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSE.Core.DomainObjects
+{
+    public class IRepository<T> : IDisposable where T : Entity, IAggregateRoot
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

13607f05c09f81c2e4a8b22dcc3d88dd4dfd231b
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 14:49:15 -0300

    Add == an != comparations

--- NerdStoreEnterprise/src/buildingBlocks/NSE.Core/DomainObjects/Entity.cs
+++ NerdStoreEnterprise/src/buildingBlocks/NSE.Core/DomainObjects/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Intrinsics.X86;
 
 namespace NSE.Core.DomainObjects
 {
@@ -14,6 +15,22 @@ namespace NSE.Core.DomainObjects
             if (ReferenceEquals(null, compareTo)) return false;
 
             return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
         }
 
         public override int GetHashCode()

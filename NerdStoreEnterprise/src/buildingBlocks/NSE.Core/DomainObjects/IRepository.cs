c27ae6075a8584c0c1dd1d07c8c41aabd02a8f06
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 16:03:14 -0300

    Change to interface

--- NerdStoreEnterprise/src/buildingBlocks/NSE.Core/DomainObjects/IRepository.cs
+++ NerdStoreEnterprise/src/buildingBlocks/NSE.Core/DomainObjects/IRepository.cs
@@ -2,11 +2,8 @@
 
 namespace NSE.Core.DomainObjects
 {
-    public class IRepository<T> : IDisposable where T : Entity, IAggregateRoot
+    public interface IRepository<T> : IDisposable where T : Entity, IAggregateRoot
     {
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }

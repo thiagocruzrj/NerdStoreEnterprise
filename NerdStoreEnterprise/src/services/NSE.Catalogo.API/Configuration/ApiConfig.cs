b0c28833fecf0be5601052423b5db6365135887d
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 16:53:22 -0300

    Create SwaggerConfig

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Data;
-using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Catalogo.API.Configuration
 {

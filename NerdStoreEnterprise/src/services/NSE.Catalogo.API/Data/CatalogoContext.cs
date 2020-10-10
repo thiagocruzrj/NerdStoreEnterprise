fe299c6ddf5df78f3efab3dc973d16c0d96ad98c
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 16:31:20 -0300

    Implement commit

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace NSE.Catalogo.API.Data
 {
-    public class CatalogoContext : DbContext
+    public class CatalogoContext : DbContext, IUnitOfWork
     {
         public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
 
@@ -17,6 +19,11 @@ namespace NSE.Catalogo.API.Data
                 property.SetColumnType("varchar(250)");
 
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
         }
     }
 }

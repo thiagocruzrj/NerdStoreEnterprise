3dab2f9ea319f1fb3942fa7fc02c4dcbb5aa95ca
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 14:58:54 -0300

    Mapping produtos tables

--- NerdStoreEnterprise/src/services/NSE.Catalog.API/Data/Mappings/ProdutoMapping.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalog.API/Data/Mappings/ProdutoMapping.cs
@@ -8,7 +8,21 @@ namespace NSE.Catalog.API.Data
     {
         public void Configure(EntityTypeBuilder<Produto> builder)
         {
-            throw new System.NotImplementedException();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.ToTable("Produtos");
         }
     }
 }

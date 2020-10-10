3807845d0e8a228d5c73ef05c2c2089def47561d
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 16:06:25 -0300

    Create ProdutoRepository.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,36 @@
+﻿using NSE.Catalogo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        public Task<IEnumerable<Produto>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Produto> ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

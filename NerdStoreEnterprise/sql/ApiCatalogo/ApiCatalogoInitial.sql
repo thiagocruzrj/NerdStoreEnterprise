d46c1f248ae0af275ec23f1f22483ba7fcf83f80
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 15:58:18 -0300

    Create ApiCatalogoInitial.sql

---
+++ NerdStoreEnterprise/sql/ApiCatalogo/ApiCatalogoInitial.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Produtos] (
+    [Id] uniqueidentifier NOT NULL,
+    [Nome] varchar(250) NOT NULL,
+    [Descricao] varchar(250) NOT NULL,
+    [Ativo] bit NOT NULL,
+    [Valor] decimal(18,2) NOT NULL,
+    [DataCadastro] datetime2 NOT NULL,
+    [Imagem] varchar(250) NOT NULL,
+    [QuantidadeEstoque] int NOT NULL,
+    CONSTRAINT [PK_Produtos] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20201010185657_initial', N'3.1.3');
+
+GO
+

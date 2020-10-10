8a78dd96fecea7f50241a403971f6dd61aae38f7
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 14:57:25 -0300

    Create ProdutoMapping.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Catalog.API/Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Catalog.API.Models;
+
+namespace NSE.Catalog.API.Data
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

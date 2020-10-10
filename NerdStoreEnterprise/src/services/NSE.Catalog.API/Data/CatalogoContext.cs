e50f22287fd8f85785af961ebcd444cd25706ff0
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 14:56:08 -0300

    Create CatalogoContext.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Catalog.API/Data/CatalogoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalog.API.Models;
+
+namespace NSE.Catalog.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}

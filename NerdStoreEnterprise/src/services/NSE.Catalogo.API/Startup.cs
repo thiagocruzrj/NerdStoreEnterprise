eef269342f94cfc722a22b621fc66bd6a2d2e03c
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 21:03:46 -0300

    Update Startup.cs

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Startup.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Configuration;
+using NSE.WebApi.Core.Identidade;
 
 namespace NSE.Catalogo.API
 {
@@ -30,16 +31,14 @@ namespace NSE.Catalogo.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApiConfiguration(Configuration);
-
+            services.AddJwtConfiguration(Configuration);
             services.AddSwaggerConfiguration();
-
             services.RegisterServices();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwaggerConfiguration();
-
             app.UseApiConfiguration(env);
         }
     }

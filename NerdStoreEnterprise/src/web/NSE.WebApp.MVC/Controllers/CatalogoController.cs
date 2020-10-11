158d20e75d299a2e45c67029d18fa993305ab38d
Thiago Cruz <thagocruz@gmail.com>
2020-10-11 12:46:53 -0300

    Update CatalogoController.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using NSE.WebApp.MVC.Services;
-using System;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Services;
 
 namespace NSE.WebApp.MVC.Controllers
 {

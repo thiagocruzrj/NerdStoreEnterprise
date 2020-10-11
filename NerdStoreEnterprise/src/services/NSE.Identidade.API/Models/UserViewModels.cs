8d2e0640d7936a2617083c4953ccc7934051553e
Thiago Cruz <thagocruz@gmail.com>
2020-10-11 12:44:31 -0300

    Update UserViewModels.cs

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Models/UserViewModels.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace NSE.Identidade.API.Models
@@ -7,21 +6,21 @@ namespace NSE.Identidade.API.Models
     public class UsuarioRegistro
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [EmailAddress(ErrorMessage = "O campo {0} está em formato invalido")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Senha { get; set; }
 
-        [Compare("Senha", ErrorMessage = "As senhas nao conferem")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem.")]
         public string SenhaConfirmacao { get; set; }
     }
 
     public class UsuarioLogin
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [EmailAddress(ErrorMessage = "O campo {0} está em formato invalido")]
+        [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]

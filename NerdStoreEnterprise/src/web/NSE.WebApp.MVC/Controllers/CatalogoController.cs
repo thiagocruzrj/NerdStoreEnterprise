eb97defd35fdbf533a8099b569635803ff88416c
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 22:13:54 -0300

    Create CatalogoController.cs

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : MainController
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}

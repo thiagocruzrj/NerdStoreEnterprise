33eee9ae266fe38b71084593d4df3b1e3333ce47
Thiago Cruz <thagocruz@gmail.com>
2020-10-11 12:35:57 -0300

    Update CatalogoController.cs

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using NSE.Catalogo.API.Models;
-using NSE.WebApi.Core.Identidade;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Catalogo.API.Models;
+using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Catalogo.API.Controllers
 {
@@ -26,7 +26,7 @@ namespace NSE.Catalogo.API.Controllers
             return await _produtoRepository.ObterTodos();
         }
 
-        [ClaimsAuthorize("Catalogo", "Ler")]
+        [ClaimsAuthorize("Catalogo","Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {

01d689ae8c1ddc0579ba3289dd862c00964017d5
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 21:04:27 -0300

    Update ApiConfig.cs

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Catalogo.API.Data;
+using NSE.WebApi.Core.Identidade;
 
 namespace NSE.Catalogo.API.Configuration
 {
@@ -38,6 +39,8 @@ namespace NSE.Catalogo.API.Configuration
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthConfiguration();
 
             app.UseCors("Total");
 

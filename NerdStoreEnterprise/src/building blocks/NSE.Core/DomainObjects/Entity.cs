f385d9fdc0bf6d7ca802a0087826cfddce93a594
Thiago Cruz <thagocruz@gmail.com>
2020-10-11 12:51:06 -0300

    Update Entity.cs

--- NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
@@ -11,18 +11,16 @@ namespace NSE.Core.DomainObjects
             var compareTo = obj as Entity;
 
             if (ReferenceEquals(this, compareTo)) return true;
-            if (ReferenceEquals(null, compareTo)) return false;
+            if (compareTo is null) return false;
 
             return Id.Equals(compareTo.Id);
         }
 
         public static bool operator ==(Entity a, Entity b)
         {
-            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
-                return true;
+            if (a is null && b is null) return true;
 
-            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
-                return false;
+            if (a is null || b is null) return false;
 
             return a.Equals(b);
         }

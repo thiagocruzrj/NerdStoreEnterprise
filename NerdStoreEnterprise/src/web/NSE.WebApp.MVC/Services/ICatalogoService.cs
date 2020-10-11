a8f7accf199f05cfb1e7733ce0694b5d8500156b
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 22:25:30 -0300

    Change CatalogService class location

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Extensions.Options;
-using NSE.WebApp.MVC.Extensions;
-using NSE.WebApp.MVC.Models;
+﻿using NSE.WebApp.MVC.Models;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
@@ -12,32 +9,5 @@ namespace NSE.WebApp.MVC.Services
     {
         Task<IEnumerable<ProdutoViewModel>> ObterTodos();
         Task<ProdutoViewModel> ObterPorId(Guid id);
-    }
-
-    public class CatalogoService : Service, ICatalogoService
-    {
-        private readonly HttpClient _httpClient;
-
-        public CatalogoService(HttpClient httpClient, IOptions<AppSettings> settings)
-        {
-            httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
-            _httpClient = httpClient;
-        }
-
-        public async Task<ProdutoViewModel> ObterPorId(Guid id)
-        {
-            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
-            TratarErrosResponse(response);
-
-            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
-        }
-
-        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
-        {
-            var response = await _httpClient.GetAsync($"/catalogo/produtos/");
-            TratarErrosResponse(response);
-
-            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
-        }
     }
 }

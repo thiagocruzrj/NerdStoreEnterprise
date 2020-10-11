59149bc117b60ac6154495865ebf376ab6bc870d
Thiago Cruz <thagocruz@gmail.com>
2020-10-11 12:41:43 -0300

    Update ApiConfig.cs

--- NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NSE.WebApi.Core.Identidade;
+using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Identidade.API.Configuration
 {

669607b077b045a0f895e96f57be21e73d0e187c
Thiago Cruz <thagocruz@gmail.com>
2020-10-10 22:26:43 -0300

    Update DependencyInjectionConfig.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -14,6 +14,7 @@ namespace NSE.WebApp.MVC.Configuration
             services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+            services.AddHttpClient<ICatalogoService, CatalogoService>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IUser, AspNetUser>();

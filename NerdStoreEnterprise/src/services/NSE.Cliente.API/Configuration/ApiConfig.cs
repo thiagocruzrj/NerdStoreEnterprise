1a14666f8995d12beba2402ae921f997aa30c64f
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:38:24 -0300

    Create ApiConfig.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/ApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Cliente.API.Data;
+
+namespace NSE.Cliente.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ClienteContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+    }
+}

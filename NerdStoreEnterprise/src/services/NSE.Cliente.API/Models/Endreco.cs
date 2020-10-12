240342ab1a792515a3d5a7200780cb9db962e290
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 15:04:17 -0300

    Update Endreco.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Endreco.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Endreco.cs
@@ -1,10 +1,22 @@
-﻿using NSE.Core.DomainObjects;
-using System;
+﻿using System;
+using NSE.Core.DomainObjects;
 
 namespace NSE.Clientes.API.Models
 {
     public class Endereco : Entity
     {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        // EF Relation
+        public Cliente Cliente { get; protected set; }
+
         public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
         {
             Logradouro = logradouro;
@@ -15,20 +27,5 @@ namespace NSE.Clientes.API.Models
             Cidade = cidade;
             Estado = estado;
         }
-
-        // EF Relation
-        protected Endereco() { }
-
-        public Guid ClientId { get; private set; }
-        public string Logradouro { get; private set; }
-        public string Numero { get; private set; }
-        public string Complemento { get; private set; }
-        public string Bairro { get; private set; }
-        public string Cep { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
-
-        // EF Relation
-        public Cliente Cliente { get; private set; }
     }
 }

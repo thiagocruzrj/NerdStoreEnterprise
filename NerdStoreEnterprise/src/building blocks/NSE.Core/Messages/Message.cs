8a0046f061b8fde36e9860236988a08bfb26a9f7
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 15:14:53 -0300

    Create Message.cs

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Message
+    {
+        public string MessageType { get; private set; }
+        public Guid AggregateId { get; private set; }
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}

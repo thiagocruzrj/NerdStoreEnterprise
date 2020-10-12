e1140cdc677e6a1aae19ffe0c9b15b9a33954f59
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 12:35:28 -0300

    Add ctor and mark as Aggregate

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -1,7 +1,17 @@
-﻿namespace NSE.Cliente.API.Models
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
 {
-    public class Cliente
+    public class Cliente : Entity, IAggregateRoot
     {
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+
         public string Nome { get; private set; }
         public string Email { get; private set; }
         public string Cpf { get; private set; }
@@ -9,7 +19,7 @@
         public Endreco Endreco { get; private set; }
     }
 
-    public class Endreco
+    public class Endreco : Entity
     { 
     }
 }

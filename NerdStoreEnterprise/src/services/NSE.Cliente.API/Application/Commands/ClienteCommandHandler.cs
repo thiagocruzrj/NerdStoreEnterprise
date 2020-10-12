6393944d144e1671b1db281c5e39daf77ce67620
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 16:18:54 -0300

    Add base validation

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,6 +1,17 @@
-﻿namespace NSE.Clientes.API.Application.Commands
+﻿using FluentValidation.Results;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
 {
-    public class ClienteCommandHandler
+    public class ClienteCommandHandler : IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido()) return message.ValidationResult;
+
+            return message.ValidationResult;
+        }
     }
 }

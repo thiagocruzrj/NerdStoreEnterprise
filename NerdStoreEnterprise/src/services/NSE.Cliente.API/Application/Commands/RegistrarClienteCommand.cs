bf31f47b9b901bbfddb7df39fcf6cb07dfba9735
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 17:15:46 -0300

    Add RegistrarClienteValidation

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -31,7 +31,25 @@ namespace NSE.Clientes.API.Application.Commands
 
             RuleFor(c => c.Nome)
                 .NotEmpty()
-                .WithMessage("O nome do cliente não foi informado");
+                .WithMessage("O nome do cliente não foi informado"); 
+            
+            RuleFor(c => c.Cpf)
+                 .Must(TerCpfValido)
+                 .WithMessage("O CPF informado não é válido.");
+
+            RuleFor(c => c.Email)
+                .Must(TerEmailValido)
+                .WithMessage("O e-mail informado não é válido.");
+        }
+
+        protected static bool TerCpfValido(string cpf)
+        {
+            return Core.DomainObjects.Cpf.Validar(cpf);
+        }
+
+        protected static bool TerEmailValido(string email)
+        {
+            return Core.DomainObjects.Email.Validar(email);
         }
     }
 }

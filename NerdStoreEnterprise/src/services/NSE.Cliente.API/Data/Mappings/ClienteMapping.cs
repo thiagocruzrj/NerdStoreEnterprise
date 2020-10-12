492c1d2e02468e52fa005cf6ce7daccffbfd74c6
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:33:28 -0300

    Update ClienteMapping.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Core.DomainObjects;
 using System;
 
 namespace NSE.Cliente.API.Data.Mappings
@@ -8,7 +9,34 @@ namespace NSE.Cliente.API.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<Models.Cliente> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Numero)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            });
+
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Endereco)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.EnderecoMaxLength})");
+            });
+
+            // 1 : 1 => Aluno : Endereco
+            builder.HasOne(c => c.Endereco)
+                .WithOne(c => c.Cliente);
+
+            builder.ToTable("Clientes");
         }
     }
 }

533471cbe64d45999a02573cd27a5f1a3bda4807
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 15:19:21 -0300

    Add ValidationResult

--- NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Command.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Command.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 
 namespace NSE.Core.Messages
 {
-    public abstract class Command
+    public abstract class Command : Message
     {
         public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+
+        }
+
+        public virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

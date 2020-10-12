f04757d1fe1122d7bc577facac026e0bbd5d4057
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 12:53:46 -0300

    Create Cpf.cs

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Cpf.cs
@@ -0,0 +1,16 @@
+﻿namespace NSE.Core.DomainObjects
+{
+    public class Cpf
+    {
+        public const int CpfMaxLength = 11;
+        public string Numero { get; private set; }
+
+        // CTOR do EF
+        protected Cpf() { }
+
+        public Cpf(string numero)
+        {
+            Numero = numero;
+        }
+    }
+}

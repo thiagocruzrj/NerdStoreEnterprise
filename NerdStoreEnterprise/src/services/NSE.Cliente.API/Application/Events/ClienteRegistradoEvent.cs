e40a7237f1f8770d2bcd685c42c7d2da5af392e1
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 17:31:41 -0300

    Create ClienteRegistradoEvent.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Events/ClienteRegistradoEvent.cs
@@ -0,0 +1,22 @@
+﻿using NSE.Core.Mediator;
+using System;
+
+namespace NSE.Clientes.API.Application.Events
+{
+    public class ClienteRegistradoEvent : Event
+    {
+        public ClienteRegistradoEvent(Guid id, string nome, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}

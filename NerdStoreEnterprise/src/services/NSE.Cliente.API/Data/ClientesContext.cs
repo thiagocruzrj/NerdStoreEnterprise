82d59e8c310af5a23350dcf8a479fbc6c460e2ee
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:23:24 -0300

    Create ClientesContext.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Cliente.API.Models;
+using NSE.Core.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Data
+{
+    public class ClientesContext : DbContext, IUnitOfWork
+    {
+        public ClientesContext(DbContextOptions<ClientesContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Models.Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}

476bf7b1c7d05b996868eb365a594f73e0067002
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:32:55 -0300

    Update Cliente.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -1,10 +1,19 @@
-﻿using NSE.Core.DomainObjects;
-using System;
+﻿using System;
+using NSE.Core.DomainObjects;
 
 namespace NSE.Cliente.API.Models
 {
     public class Cliente : Entity, IAggregateRoot
     {
+        public string Nome { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        // EF Relation
+        protected Cliente() { }
+
         public Cliente(Guid id, string nome, string email, string cpf)
         {
             Id = id;
@@ -14,21 +23,12 @@ namespace NSE.Cliente.API.Models
             Excluido = false;
         }
 
-        // EF Relation
-        protected Cliente() { }
-
-        public string Nome { get; private set; }
-        public Email Email { get; private set; }
-        public Cpf Cpf { get; private set; }
-        public bool Excluido { get; private set; }
-        public Endereco Endereco { get; private set; }
-
         public void TrocarEmail(string email)
         {
             Email = new Email(email);
         }
 
-        public void AlterarEndereco(Endereco endereco)
+        public void AtribuirEndereco(Endereco endereco)
         {
             Endereco = endereco;
         }

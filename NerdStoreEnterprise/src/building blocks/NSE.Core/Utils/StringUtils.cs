3faa6352df67bc208ac3d0ae64460cad178374ce
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 13:03:06 -0300

    Create StringUtils.cs

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Utils/StringUtils.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace NSE.Core.Utils
+{
+    public static class StringUtils
+    {
+        public static string ApenasNumeros(this string str, string input) => new string(input.Where(char.IsDigit).ToArray());
+    }
+}

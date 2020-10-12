366fe3cfa615be0e2e203219d3d80a47d7b2f2ef
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 16:50:03 -0300

    Create ClienteRepository.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,30 @@
+﻿using NSE.Clientes.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Data.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        public void Adicionar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cliente> ObterPorCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Cliente>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

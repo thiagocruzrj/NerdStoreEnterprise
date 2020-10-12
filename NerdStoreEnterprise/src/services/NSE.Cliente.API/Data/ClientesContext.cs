ce7518e2aaf94f6e06b95edebfd9960a89af3341
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:23:45 -0300

    Override OnModelCreating

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/ClientesContext.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -19,5 +19,18 @@ namespace NSE.Cliente.API.Data
 
         public DbSet<Models.Cliente> Clientes { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+        }
     }
 }

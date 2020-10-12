2f2ee6065309c0c89b3c53700c2746eb290a1fb7
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 15:10:45 -0300

    Create RegistrarClienteCommand.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class RegistrarClienteCommand
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
+}

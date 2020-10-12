ad506e5f066e9b0222484a6761e4ecf7740509f1
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:32:50 -0300

    Create ClienteMapping.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace NSE.Cliente.API.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Models.Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Models.Cliente> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

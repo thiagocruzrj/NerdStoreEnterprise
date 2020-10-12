6bffbc3a462f85e8793451b96fd43ba2cec62cd4
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 12:36:33 -0300

    Create Endreco entity

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -20,6 +20,24 @@ namespace NSE.Cliente.API.Models
     }
 
     public class Endreco : Entity
-    { 
+    {
+        public Endreco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
     }
 }

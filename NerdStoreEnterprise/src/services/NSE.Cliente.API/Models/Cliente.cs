c013ab3fb6b1fb7ecd761d93c57d9e11133055b8
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 12:40:17 -0300

    Move endereço

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -1,5 +1,4 @@
 ﻿using NSE.Core.DomainObjects;
-using System;
 
 namespace NSE.Cliente.API.Models
 {
@@ -21,34 +20,5 @@ namespace NSE.Cliente.API.Models
         public string Cpf { get; private set; }
         public bool Excluido { get; private set; }
         public Endreco Endreco { get; private set; }
-    }
-
-    public class Endreco : Entity
-    {
-        public Endreco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
-        {
-            Logradouro = logradouro;
-            Numero = numero;
-            Complemento = complemento;
-            Bairro = bairro;
-            Cep = cep;
-            Cidade = cidade;
-            Estado = estado;
-        }
-
-        // EF Relation
-        protected Endreco() { }
-
-        public Guid ClientId { get; private set; }
-        public string Logradouro { get; private set; }
-        public string Numero { get; private set; }
-        public string Complemento { get; private set; }
-        public string Bairro { get; private set; }
-        public string Cep { get; private set; }
-        public string Cidade { get; private set; }
-        public string Estado { get; private set; }
-
-        // EF Relation
-        public Cliente Cliente { get; private set; }
     }
 }
---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Endreco.cs
@@ -0,0 +1,34 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Endreco : Entity
+    {
+        public Endreco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+
+        // EF Relation
+        protected Endreco() { }
+
+        public Guid ClientId { get; private set; }
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+
+        // EF Relation
+        public Cliente Cliente { get; private set; }
+    }
+}

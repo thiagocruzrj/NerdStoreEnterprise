5acc8b8db50b333783c9584bdea24ea54a435e93
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 12:32:24 -0300

    Create Cliente.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace NSE.Cliente.API.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endreco Endreco { get; private set; }
+    }
+
+    public class Endreco
+    { 
+    }
+}

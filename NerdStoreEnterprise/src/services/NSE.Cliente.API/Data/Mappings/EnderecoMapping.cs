8d7c9966cd8a5ac0a601521e7fa79dbad1508df3
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:33:58 -0300

    Create EnderecoMapping.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Cliente.API.Models;
+using System;
+
+namespace NSE.Cliente.API.Data.Mappings
+{
+    public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

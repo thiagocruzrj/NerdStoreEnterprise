c2a91389e9d8123aa1ac05f23c7bd9f28ccbfe5a
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 14:34:11 -0300

    Update EnderecoMapping.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/EnderecoMapping.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Data/Mappings/EnderecoMapping.cs
@@ -9,7 +9,36 @@ namespace NSE.Cliente.API.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<Endereco> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Logradouro)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(c => c.Numero)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(c => c.Cep)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            builder.Property(c => c.Complemento)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Bairro)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Cidade)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.Estado)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("Enderecos");
         }
     }
 }

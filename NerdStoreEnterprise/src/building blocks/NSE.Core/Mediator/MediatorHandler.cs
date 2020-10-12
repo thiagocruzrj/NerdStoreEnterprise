131b9f47b175c9e94acab28de7b345ff49a3eca0
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 16:06:00 -0300

    Create MediatorHandler.cs

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Mediator/MediatorHandler.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+using NSE.Core.Messages;
+using System.Threading.Tasks;
+
+namespace NSE.Core.Mediator
+{
+    public class MediatorHandler : IMediatorHandler
+    {
+        public Task<ValidationResult> EnviarComando<T>(T comando) where T : Command
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task PublicarEvento<T>(T evento) where T : Event
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

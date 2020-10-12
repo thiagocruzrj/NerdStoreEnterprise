e7faa09ca617962cb8345aad9839babb0c195c97
Thiago Cruz <thagocruz@gmail.com>
2020-10-12 17:00:52 -0300

    Using unitofwork to error verify

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -9,11 +9,29 @@ namespace NSE.Clientes.API.Application.Commands
 {
     public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteCommandHandler(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
         public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
         {
             if (!message.EhValido()) return message.ValidationResult;
 
             var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+            var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
+
+            if(clienteExistente != null)
+            {
+                AdicionarErro("Este CPF já está em uso.");
+                return ValidationResult;
+            }
+
+            _clienteRepository.Adicionar(cliente);
+
+            if (!await _clienteRepository.UnitOfWork.Commit()) AdicionarErro("Houve um erro ao persistir o novo cliente na base.");
 
             return message.ValidationResult;
         }

cf64af70d098df60133d24463c308bc516de532f
Thiago Cruz <thagocruz@gmail.com>
2020-10-14 22:41:47 -0300

    Validating cpf

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/CpfAnnotation.cs
@@ -0,0 +1,11 @@
+﻿using NSE.Core.DomainObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class CpfAnnotation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext) =>
+            Cpf.Validar(value.ToString()) ? ValidationResult.Success : new ValidationResult("Cpf em formato inválido");
+    }
+}

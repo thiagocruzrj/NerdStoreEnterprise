f56ca200a2df8f816491402f7d2e9ade7ef64deb
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 17:31:54 -0300

    Create RegistroClienteIntegrationHandler.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Services
+{
+    public class RegistroClienteIntegrationHandler : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

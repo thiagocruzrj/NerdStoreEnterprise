803eb6901fb344c78751160a298f8d05f873c134
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 20:51:53 -0300

    Create DependencyInjectionExtensions.cs

---
+++ NerdStoreEnterprise/src/building blocks/NSE.MessageBus/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.MessageBus
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static void AddMessageBus(this IServiceCollection services, string connection)
+        {
+
+        }
+    }
+}

0570d4e2a7f6b41537ff1d0176d3ca43b3e7153f
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 17:53:50 -0300

    Add service locator scoped

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
@@ -1,7 +1,9 @@
 ﻿using EasyNetQ;
 using FluentValidation.Results;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.Clientes.API.Application.Commands;
+using NSE.Core.Mediator;
 using NSE.Core.Messages.Integration;
 using System;
 using System.Threading;
@@ -12,6 +14,12 @@ namespace NSE.Clientes.API.Services
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
         private IBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -25,6 +33,15 @@ namespace NSE.Clientes.API.Services
         private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
         {
             var clienteCommand = new RegistrarClienteCommand(message.Id, message.Nome, message.Email, message.Cpf);
+            ValidationResult sucesso;
+
+            using(var scope = _serviceProvider.CreateScope())
+            {
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
+                sucesso = await mediator.EnviarComando(clienteCommand);
+            }
+
+            return sucesso;
         }
     }
 }

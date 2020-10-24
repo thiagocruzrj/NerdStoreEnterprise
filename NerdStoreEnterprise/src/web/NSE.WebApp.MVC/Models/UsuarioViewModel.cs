c7b204d9cfd806512eeef74a9dab0c1407a6b7bb
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 16:53:48 -0300

    Update UsuarioViewModel.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using NSE.WebApp.MVC.Extensions;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 

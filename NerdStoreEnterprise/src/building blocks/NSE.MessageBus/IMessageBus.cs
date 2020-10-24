4cbd204311b2abe64aae4e430c2a7dc25c365cec
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 20:05:07 -0300

    Add publish subscribe methods

--- NerdStoreEnterprise/src/building blocks/NSE.MessageBus/IMessageBus.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.MessageBus/IMessageBus.cs
@@ -1,6 +1,14 @@
-﻿namespace NSE.MessageBus
+﻿using NSE.Core.Messages.Integration;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.MessageBus
 {
-    public interface IMessageBus
+    public interface IMessageBus : IDisposable
     {
+        void Publish<T>(T message) where T : IntegrationEvent;
+        Task PublishAsync<T>(T message) where T : IntegrationEvent;
+        void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+        void SubscribeAsync<T>(string subscriptionId, Action<T, Task> onMessage) where T : class;
     }
 }

add806f3b2e9302329ae4f8e338bb45fa2de6c99
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 17:44:42 -0300

    Add RegistrarCliente

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using EasyNetQ;
+using FluentValidation.Results;
+using Microsoft.Extensions.Hosting;
+using NSE.Core.Messages.Integration;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,9 +10,25 @@ namespace NSE.Clientes.API.Services
 {
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
+        private IBus _bus;
+
+        public RegistroClienteIntegrationHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            _bus = RabbitHutch.CreateBus("host=localhost:5672");
+            _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request =>
+                new ResponseMessage(await RegistrarCliente(request)));
+
+            return Task.CompletedTask;
+        }
+
+        private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
+        {
+
         }
     }
 }

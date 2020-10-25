9dd4d2362e5d9a51d13f189af2b4984e7b563e44
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 21:14:34 -0300

    Update MessageBusConfig.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/MessageBusConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/MessageBusConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NSE.Core.Utils;
+using NSE.MessageBus;
 
 namespace NSE.Clientes.API.Configuration
 {
@@ -7,7 +9,7 @@ namespace NSE.Clientes.API.Configuration
     {
         public static void AddMessageBusConfigurarion(this IServiceCollection services, IConfiguration configuration)
         {
-
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
         }
     }
 }

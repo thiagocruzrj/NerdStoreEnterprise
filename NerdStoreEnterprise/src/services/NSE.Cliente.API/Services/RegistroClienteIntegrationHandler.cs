81361d0f0a36791e6350215d7df6911b63449c1e
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 21:41:43 -0300

    Update RegistroClienteIntegrationHandler.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Services/RegistroClienteIntegrationHandler.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Hosting;
 using NSE.Clientes.API.Application.Commands;
 using NSE.Core.Mediator;
 using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace NSE.Clientes.API.Services
 {
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
-        private IBus _bus;
+        private IMessageBus _bus;
         private readonly IServiceProvider _serviceProvider;
 
         public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
@@ -23,7 +24,6 @@ namespace NSE.Clientes.API.Services
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _bus = RabbitHutch.CreateBus("host=localhost:5672");
             _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request =>
                 new ResponseMessage(await RegistrarCliente(request)));
 

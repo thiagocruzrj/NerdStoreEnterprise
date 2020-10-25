9d29c7dae15adf33032760464e47fcbc2605b59b
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 21:25:13 -0300

    Update DependencyInjectionConfig.cs

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -21,8 +21,6 @@ namespace NSE.Clientes.API.Configuration
             
             services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddScoped<ClientesContext>();
-
-            services.AddHostedService<RegistroClienteIntegrationHandler>();
         }
     }
 }

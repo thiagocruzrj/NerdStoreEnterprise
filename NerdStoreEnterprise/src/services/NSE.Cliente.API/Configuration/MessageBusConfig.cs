0935e67fac8b857c21f91bdb476df842fc3b8945
Thiago Cruz <thagocruz@gmail.com>
2020-10-24 21:08:04 -0300

    Create MessageBusConfig.cs

---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Clientes.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfigurarion(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}

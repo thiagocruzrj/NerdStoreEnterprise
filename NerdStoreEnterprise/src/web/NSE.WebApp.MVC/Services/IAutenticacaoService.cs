bf8b32522e2469c6c0a7676e8ef25182f73c5c68
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:25:09 -0300

    Update IAutenticacaoService.cs

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -6,6 +6,7 @@ namespace NSE.WebApp.MVC.Services
     public interface IAutenticacaoService
     {
         Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+
         Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
     }
 }

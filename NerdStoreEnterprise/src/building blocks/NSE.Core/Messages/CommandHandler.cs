28e0d8279926d4b1a70c104a98a8c7c19f31fedf
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:15:32 -0300

    Update CommandHandler.cs

--- NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/CommandHandler.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/CommandHandler.cs
@@ -1,6 +1,6 @@
-﻿using FluentValidation.Results;
+﻿using System.Threading.Tasks;
+using FluentValidation.Results;
 using NSE.Core.Data;
-using System.Threading.Tasks;
 
 namespace NSE.Core.Messages
 {
@@ -13,14 +13,15 @@ namespace NSE.Core.Messages
             ValidationResult = new ValidationResult();
         }
 
-        protected void AdicionarErro(string mensage)
+        protected void AdicionarErro(string mensagem)
         {
-            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, mensage));
+            ValidationResult.Errors.Add(new ValidationFailure(string.Empty, mensagem));
         }
 
         protected async Task<ValidationResult> PersistirDados(IUnitOfWork uow)
         {
-            if (!await uow.Commit()) AdicionarErro("Houve um erro ao persistir os dados.");
+            if (!await uow.Commit()) AdicionarErro("Houve um erro ao persistir os dados");
+
             return ValidationResult;
         }
     }

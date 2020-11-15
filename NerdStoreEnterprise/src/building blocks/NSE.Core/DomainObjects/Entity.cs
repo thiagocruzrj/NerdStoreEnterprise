5eda8fb515577e94e1cd586a9b2fb041058ab443
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:15:18 -0300

    Update Entity.cs

--- NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/Entity.cs
@@ -1,6 +1,6 @@
-﻿using NSE.Core.Mediator;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using NSE.Core.Messages;
 
 namespace NSE.Core.DomainObjects
 {
@@ -8,12 +8,17 @@ namespace NSE.Core.DomainObjects
     {
         public Guid Id { get; set; }
 
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
         private List<Event> _notificacoes;
         public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
 
         public void AdicionarEvento(Event evento)
         {
-            _notificacoes ??= new List<Event>();
+            _notificacoes = _notificacoes ?? new List<Event>();
             _notificacoes.Add(evento);
         }
 
@@ -27,28 +32,44 @@ namespace NSE.Core.DomainObjects
             _notificacoes?.Clear();
         }
 
+        #region Comparações
+
         public override bool Equals(object obj)
         {
             var compareTo = obj as Entity;
 
             if (ReferenceEquals(this, compareTo)) return true;
-            if (compareTo is null) return false;
+            if (ReferenceEquals(null, compareTo)) return false;
 
             return Id.Equals(compareTo.Id);
         }
 
         public static bool operator ==(Entity a, Entity b)
         {
-            if (a is null && b is null) return true;
-            if (a is null || b is null) return false;
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
 
             return a.Equals(b);
         }
 
-        public static bool operator !=(Entity a, Entity b) => !(a == b);
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
 
-        public override int GetHashCode() => (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
 
-        public override string ToString() => $"{GetType().Name} [Id={Id}]";
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={Id}]";
+        }
+
+        #endregion
     }
 }

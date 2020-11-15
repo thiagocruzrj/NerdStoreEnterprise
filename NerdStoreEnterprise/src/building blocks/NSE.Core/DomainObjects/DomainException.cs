fa812e446785c0426ab6b6a94bac941771a10a80
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:15:14 -0300

    Update DomainException.cs

--- NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/DomainException.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/DomainException.cs
@@ -4,8 +4,13 @@ namespace NSE.Core.DomainObjects
 {
     public class DomainException : Exception
     {
-        public DomainException() { }
-        public DomainException(string message) : base(message) { }
-        public DomainException(string message, Exception innerException) : base(message, innerException) { }
+        public DomainException()
+        { }
+
+        public DomainException(string message) : base(message)
+        { }
+
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        { }
     }
 }

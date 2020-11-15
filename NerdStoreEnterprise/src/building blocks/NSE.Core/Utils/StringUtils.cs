fb24174f20e691dd7f801a2e347daee916abc87b
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:16:15 -0300

    Update StringUtils.cs

--- NerdStoreEnterprise/src/building blocks/NSE.Core/Utils/StringUtils.cs
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Utils/StringUtils.cs
@@ -4,6 +4,9 @@ namespace NSE.Core.Utils
 {
     public static class StringUtils
     {
-        public static string ApenasNumeros(this string str, string input) => new string(input.Where(char.IsDigit).ToArray());
+        public static string ApenasNumeros(this string str, string input)
+        {
+            return new string(input.Where(char.IsDigit).ToArray());
+        }
     }
 }

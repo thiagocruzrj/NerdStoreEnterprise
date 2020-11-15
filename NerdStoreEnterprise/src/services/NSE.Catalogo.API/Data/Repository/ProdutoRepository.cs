4d7df15497b91512f53ae64105ae57cd9b5476e2
Thiago Cruz <thagocruz@gmail.com>
2020-11-14 21:17:15 -0300

    Update ProdutoRepository.cs

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -18,13 +18,25 @@ namespace NSE.Catalogo.API.Data.Repository
 
         public IUnitOfWork UnitOfWork => _context;
 
-        public async Task<IEnumerable<Produto>> ObterTodos() => await _context.Produtos.AsNoTracking().ToListAsync();
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _context.Produtos.AsNoTracking().ToListAsync();
+        }
 
-        public async Task<Produto> ObterPorId(Guid id) => await _context.Produtos.FindAsync(id);
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
 
-        public void Adicionar(Produto produto) => _context.Produtos.Add(produto);
+        public void Adicionar(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+        }
 
-        public void Atualizar(Produto produto) => _context.Produtos.Update(produto);
+        public void Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+        }
 
         public void Dispose()
         {
